2304e12cb1dbf982399f697a7b90ce63b6223562
Алексей <C00Lman041193@gmail.com>
2017-07-19 17:14:32 +0300

    second commit

--- button.lua
+++ button.lua
@@ -1,28 +1,35 @@
-require 'class'
+require 'common'
+require 'point'
 
-Button = class(function(slf, pos, size, text, action)
-	slf.pos = pos
-	slf.size = size
-	slf.text = text
-	slf.action = action
-end)
+Button = {
+	new = function(self, pos, size, text, color, action)
+		newObj = {
+			pos = Point:new(pos[1], pos[2], pos[3]),
+			size = Point:new(size[1], size[2], size[3]),
+			text = text,
+			action = action,
+			color = Point:new(color[1], color[2], color[3])
+		}
+		self.__index = self
+		return setmetatable(newObj, self)
+	end,
+	check = function(self, pos)
+		return isInRect(pos, self.pos, self.size)
+	end,
+	click = function(self) 
+		if self.action ~= nil then
+			self.action()
+		end
+	end,
+	draw = function(self)
+		r, g, b, a = love.graphics.getColor()
 
-function Button:check(pos)
-	return pos[1] > self.pos[1] and pos[1] < self.pos[1] + self.size[1] and
-		pos[2] > self.pos[2] and pos[2] < self.pos[2] + self.size[2]
-end
+		love.graphics.setColor(self.color:get())
+		love.graphics.rectangle('fill', self.pos[1], self.pos[2], self.size[1], self.size[2])
+		love.graphics.setColor(0, 0, 0)
+		love.graphics.rectangle('line', self.pos[1], self.pos[2], self.size[1], self.size[2])
 
-function Button:click()
-	self.action()
-end
-
-function Button:draw()
-	r, g, b, a = love.graphics.getColor()
-
-	love.graphics.setColor(100, 100, 100)
-	love.graphics.rectangle('fill', slf.pos[1], slf.pos[2], slf.size[1], slf.size[2])
-	love.graphics.rectangle('line', slf.pos[1], slf.pos[2], slf.size[1], slf.size[2])
-
-	love.graphics.setColor(r, g, b, a)
-end
+		love.graphics.setColor(r, g, b, a)
+	end
+}
 
--- main.lua
+++ main.lua
@@ -1,8 +1,31 @@
 require 'point'
+require 'button'
+require 'xml2lua'
+local handler = require('xmlhandler/tree')
+
+function Btn1Click()
+	print("Btn1")
+end
 
 function love.load()
-	p1 = Point(100, 100)
-	p2 = Point(200, 200)
+	local xml = xml2lua.loadFile('menu.xml')
+	local parser = xml2lua.parser(handler)
+	parser:parse(xml)
+
+	menu = {}
+	for k,btn in pairs(handler.root.menu.Button) do
+		local pos = Point:new(tonumber(btn._attr.x), tonumber(btn._attr.y))
+		local size = Point:new(tonumber(btn._attr.w), tonumber(btn._attr.h))
+		local color = Point:new(tonumber(btn._attr.r), tonumber(btn._attr.g), tonumber(btn._attr.b))
+		local btnObj = Button:new(pos, size, btn._attr.text, color, _G[btn._attr.func])
+		table.insert(menu, btnObj)
+	end
+
+	for i,b in pairs(menu) do
+		print(i,b)
+	end
+
+	love.graphics.setBackgroundColor(255, 255, 255)
 end
 
 function love.update(dt)
@@ -14,9 +37,16 @@ function love.keypressed(key, scancode, isrepeat)
 	end
 end
 
+function love.mousepressed(x, y, button, istouch)
+	for i,b in pairs(menu) do
+		if b:check(Point:new(x,y)) then
+			b:click()
+		end
+	end
+end
+
 function love.draw()
-	love.graphics.setColor(255, 255, 255)
-	local x,y,z = p1:get()
-	love.graphics.rectangle('fill', x, y, 100, 100)
-	love.graphics.getColor(0, 0, 0)
+	for i,b in pairs(menu) do
+		b:draw()
+	end
 end
